d798c56b57a2ff2254a1269015ebf1a5e851e191
rich1143 <rich1143@morris.umn.edu>
2017-11-08 16:50:17 -0600

    Moved files from Lab 8 and implemented client side login

---
+++ express.js
@@ -0,0 +1,83 @@
+// setup
+var express=require('express'),
+mysql=require('mysql'),
+credentials=require('./credentials.json'),
+app = express(),
+port = process.env.PORT || 1337;
+
+credentials.host='ids.morris.umn.edu'; //setup database credentials
+
+var connection = mysql.createConnection(credentials); // setup the connection
+
+connection.connect(function(err){if(err){console.log(error)}});
+
+// gets buttons from database
+// sends button info to client
+app.use(express.static(__dirname + '/public'));
+app.get("/buttons",function(req,res){
+  var sql = 'SELECT * FROM mitc0417.till_buttons'; // sql query to get information from till_buttons
+  connection.query(sql,(function(res){return function(err,rows,fields){
+     if(err){console.log("We have an error:");
+             console.log(err);}
+     res.send(rows);
+  }})(res));
+});
+
+// handles click information taken in from client
+app.get("/click",function(req,res){
+  var htmlID = req.param('id');
+  console.log("htmlID " + htmlID.substring(0,1))
+  var id;
+  var sql;
+  if(htmlID.substring(0,1) == ("l")) // this is if it's a line being clicked
+  {
+    id = htmlID.substring(5,htmlID.length);
+
+    // sql query to delete rows from transaction table when corresponding row is clicked
+    sql = 'DELETE FROM mitc0417.transaction_table WHERE lineID = ' + id + ';';
+  }
+  else { // this is if it's a button being clicked
+    id = htmlID.substring(7,htmlID.length);
+
+    // upsert to transaction table when button is clicked
+    // if item is in the table, it increments quantity
+    // if item is not in table, it adds it with quanitity = 1
+    sql = 'INSERT INTO mitc0417.transaction_table (buttonID, quantity) VALUES (' + id + ', 1) ON DUPLICATE KEY UPDATE quantity = quantity + 1';
+  }
+
+  connection.query(sql,(function(res){return function(err,rows,fields){
+     if(err){console.log("We have an insertion error:");
+             console.log(err);}
+     res.send(err); // Let the upstream guy know how it went
+  }})(res));
+});
+
+// gets list from database
+// sends lsit info to client
+app.get("/list",function(req,res){
+
+  // pulls information about transaction lines from till_buttons and transaction_table
+  var sql = 'SELECT * FROM mitc0417.transaction_table LEFT JOIN mitc0417.till_buttons ON mitc0417.transaction_table.buttonID = mitc0417.till_buttons.buttonID;';
+  connection.query(sql,(function(res){return function(err,rows,fields){
+     if(err){console.log("We have an error:");
+             console.log(err);}
+
+     res.send(rows);
+  }})(res));
+});
+
+
+// verifies login with information from client
+app.get("/login",function(req,res){
+  var htmlID = req.param('id');
+  var sql = "SHOW TABLES";
+  console.log("You made it!");
+
+  connection.query(sql,(function(res){return function(err,rows,fields){
+     if(err){console.log("We have an insertion error:");
+             console.log(err);}
+     res.send(err); // Let the upstream guy know how it went
+  }})(res));
+});
+
+app.listen(port);
---
+++ public/click.js
@@ -0,0 +1,164 @@
+angular.module('register',[])
+  .controller('registerCtrl',RegisterCtrl)
+  .factory('registerApi',registerApi)
+  .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
+
+function RegisterCtrl($scope,registerApi){
+  // Authentication
+  $scope.authenticated = false;
+  $scope.loginClick=loginClick;
+
+  // Button intialization
+   $scope.buttons=[]; //Initially all was still
+   $scope.errorMessage='';
+   $scope.isLoading=isLoading;
+   $scope.refreshButtons=refreshButtons;
+   $scope.buttonClick=buttonClick;
+
+// Line intialization
+   $scope.lines=[]; //Initially all was still
+   $scope.errorMessage='';
+   $scope.isLoading=isLoading;
+   $scope.refreshLines=refreshLines;
+   $scope.lineClick=lineClick;
+   $scope.total = 0;
+
+// Universal
+   var loading = false;
+
+   function isLoading(){
+    return loading;
+   }
+   // Authentication functions
+   function loginClick(){
+    console.log("clicked it");
+     $scope.errorMessage='';
+     registerApi.clickLogin(username.value, pwd.value)
+        .success(function(){
+          $scope.authenticated = true;
+        })
+        .error(function(){$scope.errorMessage="Unable to click";});
+   };
+
+
+   // Button functions
+
+   // when called, gets buttons from api and updates $scope.buttons
+  function refreshButtons(){
+    loading=true;
+    $scope.errorMessage='';
+    registerApi.getButtons()
+      .success(function(data){
+         $scope.buttons=data;
+         loading=false;
+      })
+      .error(function () {
+          $scope.errorMessage="Unable to load Buttons:  Database request failed";
+          loading=false;
+      });
+ }
+
+// when called, gets lines from api and updates $scope.lines
+ function refreshLines(){
+   loading=true;
+   $scope.errorMessage='';
+   registerApi.getLines()
+     .success(function(data){
+        $scope.lines=data;
+        decimalinator(); // formats price for UI
+        totalPrice(); // calculates and formats price for UI
+        loading=false;
+     })
+     .error(function () {
+         $scope.errorMessage="Unable to load Lines:  Database request failed";
+         loading=false;
+     });
+}
+
+// calls the database when buttons are clicked
+  function buttonClick($event){
+     $scope.errorMessage='';
+     registerApi.clickButton($event.target.id)
+        .success(function(){
+          totalPrice(); // calculates and formats price for UI
+          refreshLines(); // gets lines from api and updates $scope.lines
+        })
+        .error(function(){$scope.errorMessage="Unable to click";});
+  }
+  refreshButtons();  //make sure the buttons are loaded
+
+// calculates and formats price for UI
+  function totalPrice(){
+    $scope.totalPrice = 0;
+    for(var i = 0; i < $scope.lines.length; i++) {
+      $scope.totalPrice += $scope.lines[i].price * $scope.lines[i].quantity;
+    }
+
+    $scope.totalPrice =  $scope.totalPrice + "";
+    console.log("clicked it");
+    if($scope.totalPrice.substring($scope.totalPrice.length - 3, $scope.totalPrice.length - 2) != ".") {
+
+      if($scope.totalPrice.substring($scope.totalPrice.length - 2, $scope.totalPrice.length - 1) == ".") { // if there is one decimal place (ex: 1.5)
+        $scope.totalPrice += "0";
+      } else { // if it is a round number (ex: 36)
+        $scope.totalPrice += ".00";
+      }
+    }
+  }
+
+// formats price for UI
+  function decimalinator() {
+    for(var i = 0; i < $scope.lines.length; i++) {
+      price = ($scope.lines[i].price * $scope.lines[i].quantity) + "";
+      if(price.substring(price.length - 3, price.length - 2) != ".") {
+
+        if(price.substring(price.length - 2, price.length - 1) == ".") { // if there is one decimal place (ex: 1.5)
+          price += "0";
+        } else { // if it is a round number (ex: 36)
+          price += ".00";
+        }
+      }
+      $scope.lines[i].displayPrice = price;
+    }
+  }
+
+// calls the database when lines are clicked
+  function lineClick($event){
+     $scope.errorMessage='';
+     registerApi.clickLine($event.target.parentElement.id)
+        .success(function(){
+          totalPrice(); // calculates and formats price for UI
+          refreshLines(); // gets lines from api and updates $scope.lines
+        })
+        .error(function(){$scope.errorMessage="Unable to click";});
+  }
+  refreshLines();  //make sure the lines are loaded
+
+}
+
+// api that holds functions for retrieving button and list information
+function registerApi($http,apiUrl){
+  return{
+    getButtons: function(){ // retreives button information from database
+      var url = apiUrl + '/buttons';
+      return $http.get(url);
+    },
+    clickButton: function(id){ // sends button click information to database
+      var url = apiUrl+'/click?id='+id;
+      return $http.get(url);
+    },
+    getLines: function(){ // retreives list information from database
+      var url = apiUrl + '/list';
+      return $http.get(url);
+    },
+    clickLine: function(id){ // sends line click information to databse
+      var url = apiUrl+'/click?id='+id;
+      return $http.get(url);
+    },
+    clickLogin: function(username,password) {
+      console.log("in here");
+      var url = apiUrl + '/login';
+      return $http.get(url);
+    }
+ };
+}
